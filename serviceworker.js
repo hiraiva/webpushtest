426ef9b16dfda6ce40d6e0e75facb39e237f196a
hiraiwa <j.hiraiwa@opt.ne.jp>
2016-03-08 23:42:28 +0900

    2nd

--- serviceworker.js
+++ serviceworker.js
@@ -1,21 +1,27 @@
-var serviceWorkerApp = {
-  onReceive: function(event) {
-    if (serviceWorkerApp.isSupported()) {
-      var messege = (event && event.data) ? event.data.text() : "No data!!!!";
-      evt.waitUntil(serviceWorkerApp.showFunc(message));
-    } else {
-      console.log("Not supported...");
+;(function() {
+  var serviceWorkerApp = {
+    onReceive: function(event) {
+      if (serviceWorkerApp.isSupported()) {
+        var promise = fetch("https://hiraiva.github.io/webpushtest/message.txt", {
+          mode: "cors",
+          credentials: "include"
+        }).then(function(response) {
+          return response.text();
+        }).then(function(message) {
+          self.registration.showNotification("WebPushTest", {
+            icon: "logo.jpg",
+            body: message,
+            vibrate: [400, 100, 400]
+          });
+        });
+        event.waitUntil(promise);
+      } else {
+        console.log("Not supported...");
+      }
+    },
+    isSupported: function() {
+      return self && self.registration && self.registration.showNotification;
     }
-  },
-  showFunc: function(payload) {
-    return self.registration.showNotification("WebPushTest", {
-      icon: "optlogo.gif",
-      body: body ? unescape(body) : "(with empty payload)",
-      vibrate: [400, 100, 400]
-    });
-  },
-  isSupported: function() {
-    return self && self.registration && self.registration.showNotification;
   }
-}
-self.addEventListener("push", serviceWorkerApp.onReceive, false);
+  self.addEventListener("push", serviceWorkerApp.onReceive, false);
+})();
